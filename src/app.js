6ba0f07b8458b08b775b5ea491ecb9640cfaee4d
Andrew Song <snappysong@gmail.com>
2021-08-24 14:22:37 -0400

    changed dotenv to always be needed

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
-if (process.env.USER) require("dotenv").config();
+//if (process.env.USER) require("dotenv").config();
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors")
 
