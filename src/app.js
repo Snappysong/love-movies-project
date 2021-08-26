7e522788bdbfe826c40054eb7c80a795882c4b86
Andrew Song <snappysong@gmail.com>
2021-08-26 15:17:23 -0400

    added header function for CORS

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
-//if (process.env.USER) require("dotenv").config();
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
 
 const moviesRouter = require("./movies/movies.router");
 const reviewsRouter = require("./reviews/reviews.router");
@@ -8,6 +7,12 @@ const theatersRouter = require("./theaters/theaters.router");
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
 const app = express();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use(cors());
 app.use(express.json());
