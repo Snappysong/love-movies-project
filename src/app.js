4f5285b6139202ce5c1da018f4859c8151a9b842
Andrew Song <snappysong@gmail.com>
2021-08-26 15:21:54 -0400

    removed CORS for test

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 
 const moviesRouter = require("./movies/movies.router");
 const reviewsRouter = require("./reviews/reviews.router");
@@ -8,13 +8,8 @@ const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
 const app = express();
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 app.use("/movies", moviesRouter);
