7b39470f3347a959849599cc94eefb5adf3d752d
Andrew Song <snappysong@gmail.com>
2021-08-31 12:43:20 -0400

    CORS changes

--- src/app.js
+++ src/app.js
@@ -12,16 +12,17 @@ const corsOptions = {
     origin: "https://ilovemovies-client.vercel.app"
 }
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
-// app.use(cors({ origin: "https://ilovemovies-client.vercel.app" }));
 // app.options('*', cors())
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use("/movies", moviesRouter);
 app.use("/reviews", reviewsRouter);
