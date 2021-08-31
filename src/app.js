83bc4c4aeb99503dffcb29f5b05c75a4f9e53f6b
Andrew Song <snappysong@gmail.com>
2021-08-31 13:17:57 -0400

    CORS options

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-
+app.use(cors());
 // const corsOptions = {
 //     origin: true,
 //     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
