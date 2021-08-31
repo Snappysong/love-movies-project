5b423de304a16722f880d44f1bb0a597701142cf
Andrew Song <snappysong@gmail.com>
2021-08-31 13:14:43 -0400

    CORS options

--- src/app.js
+++ src/app.js
@@ -11,17 +11,17 @@ const app = express();
 app.use(express.json());
 
 
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
-const corsOptions = {
-    origin: true,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-}
-app.use(cors(corsOptions));
+// const corsOptions = {
+//     origin: true,
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+// }
+// app.use(cors(corsOptions));
 // app.options('*', cors(corsOptions))
 
 app.use("/movies", moviesRouter);
