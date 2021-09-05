7838896532a434eff1a4565198801b2bb8df0c70
Andrew Song <snappysong@gmail.com>
2021-09-04 21:58:45 -0400

    CORS changes

--- src/app.js
+++ src/app.js
@@ -10,12 +10,12 @@ const notFound = require("./errors/notFound");
 const app = express();
 app.use(express.json());
 
-app.use(cors());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use(cors());
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 // app.use(cors({
 //     origin: '*',
