ccb5b2d182bd5ed6c53e469cd32036a1a940b619
Andrew Song <snappysong@gmail.com>
2021-12-08 17:25:19 -0800

    SERIOUSLY TRYING CORS FIX NOW

--- src/app.js
+++ src/app.js
@@ -1,14 +1,22 @@
+//const path = require("path");
+// require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
+
 const express = require("express");
 const cors = require("cors");
 
+const errorHandler = require("./errors/errorHandler");
+const notFound = require("./errors/notFound");
 const moviesRouter = require("./movies/movies.router");
 const reviewsRouter = require("./reviews/reviews.router");
 const theatersRouter = require("./theaters/theaters.router");
-const errorHandler = require("./errors/errorHandler");
-const notFound = require("./errors/notFound");
 
 const app = express();
+
+app.use(cors());
+
 app.use(express.json());
+
+app.options("*", cors());
 
 // app.use(cors());
 // app.use(function(req, res, next) {
